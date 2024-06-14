602107c378badc32872500d39eb822fa0ce2caad
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2024-06-14 17:09:11 +0100

    refactor of common code

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'blog_posts/index'
-  get 'blog_posts/new', to: 'blog_posts#new', as: :new_blog_post
-  get 'blog_posts/:id', to: 'blog_posts#show', as: :blog_post
-  patch 'blog_posts/:id', to: 'blog_posts#update'
-  get 'blog_posts/:id/edit', to: 'blog_posts#edit', as: :edit_blog_post
-  post 'blog_posts', to: "blog_posts#create", as: :blog_posts
+
+  resources :blog_posts
+
+  # get 'blog_posts/index'
+  # get 'blog_posts/new', to: 'blog_posts#new', as: :new_blog_post
+  # get 'blog_posts/:id', to: 'blog_posts#show', as: :blog_post
+  # patch 'blog_posts/:id', to: 'blog_posts#update'
+  # delete 'blog_posts/:id', to: 'blog_posts#destroy'
+  # get 'blog_posts/:id/edit', to: 'blog_posts#edit', as: :edit_blog_post
+  # post 'blog_posts', to: "blog_posts#create", as: :blog_posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
